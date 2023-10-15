d1e5c3f8103afffc56cb44d0388b3f9abedc7044
mamun-prodhan <mamunpr9@gmail.com>
2023-10-15 11:44:37 +0600

    final comit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+// middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
@@ -17,9 +19,13 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
-app.post("/post", (req, res) => {
+app.post("/users", (req, res) => {
   console.log("post api hiting");
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
